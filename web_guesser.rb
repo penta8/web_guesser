86aeba9c684a2c87df26599d8fdf5cf984d0cf23
penta8 <utn_etudiant@yahoo.fr>
2017-02-07 16:13:50 -0600

    First refactorization web_guesser using dictionaries

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,43 +3,40 @@ require 'sinatra/reloader'
 
 
 SECRET_NUMBER = rand(100)
+VALUES = {
+  wtl: {message: 'Way Too low!', backcolor: 'red'},
+  tl:  {message: 'Too low!', backcolor: '#ffcccc'},
+  co:  {message: 'You got it right!<br>' \
+       'The SECRET NUMBER is ' + SECRET_NUMBER.to_s,
+        backcolor: 'green'},
+  wth: {message: 'Way too high!', backcolor: 'red'},
+  th:  {message: 'Too high!', backcolor: '#ffcccc'}
+}
 
 get '/' do
   guess = params["guess"].to_i
   message = check_guess(guess)
-  backcolor = getColor(message)
 
   erb :index, :locals => {:number => SECRET_NUMBER,
-                          :message => message,
-                          :backcolor => backcolor}
-end
-
-def getColor(message)
-  if message == 'Way too high!' || message == 'Way too low!'
-    'red'
-  elsif message == 'Too high!' || message == 'Too low!'
-    '#ffcccc'
-  else
-    'green'
-  end
+                          :message => VALUES[message][:message],
+                          :backcolor => VALUES[message][:backcolor]}
 end
 
 def check_guess(guess)
   puts SECRET_NUMBER
   if SECRET_NUMBER == guess
-    "You got it right!<br>" \
-    "The SECRET NUMBER is " + SECRET_NUMBER.to_s
+    :co
   elsif guess > SECRET_NUMBER
     if guess - SECRET_NUMBER > 5
-      'Way too high!'
+      :wth
     else
-      'Too high!'
+      :th
     end
   elsif guess < SECRET_NUMBER
     if SECRET_NUMBER - guess > 5
-      'Way too low!'
+      :wtl
     else
-      'Too low!'
+      :tl
     end
   end
 end
